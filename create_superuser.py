02840fe3c3029b929032e9a336c5a753c838bd04
EVELYNADMIN <ceramicos38@gmail.com>
2025-09-01 22:57:24 -0500

    Crear superusuario automáticamente en Render

---
+++ create_superuser.py
@@ -0,0 +1,18 @@
+# create_superuser.py
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ceramico_web.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+
+username = 'EVELYNADMIN'
+email = 'ceramicos38@gmail.com'
+password = 'evelyn2025'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superusuario {username} creado correctamente")
+else:
+    print(f"Superusuario {username} ya existe")
