8c947a015b43b4f98ad9b7e86bed6589a448524a
EVELYNADMIN <ceramicos38@gmail.com>
2025-09-02 19:15:49 -0500

    Actualizaciones

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,14 @@
-"""
-WSGI config for ceramico_web project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-import sys
-
-# Ejecutar script de superusuario automáticamente
-script_path = os.path.join(os.path.dirname(__file__), 'create_superuser.py')
-if os.path.exists(script_path):
-    import runpy
-    runpy.run_path(script_path)
-
-from django.core.wsgi import get_wsgi_application
+import django
+from django.contrib.auth.models import User
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ceramico_web.settings')
+django.setup()
 
-application = get_wsgi_application()
+username = "ADMIN"
+email = "admin@ceramico.com"
+password = "evelyn2025"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("✅ Superusuario creado automáticamente")
